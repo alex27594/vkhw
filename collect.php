e7382fdd7a7598c6a4613b12c7867dee0e88a2e2
Alexander Sukhochev <alex27594@gmail.com>
2019-02-05 17:14:55 +0300

    fixed collect.php

--- collect.php
+++ collect.php
@@ -2,23 +2,30 @@
 use VK\Client\VKApiClient;
 require "vendor/autoload.php";
 
-function save_arr($filename, $arr) {
-    $encodedString = json_encode($arr);
-    file_put_contents($filename, $encodedString);
+function save_arr_csv($filename, $arr, $mode) {
+    $file = fopen($filename, $mode);
+    if ($mode == 'w') {
+        fputcsv($file, array('group', 'user'));
+    }
+    foreach($arr as $edge) {
+        fputcsv($file, $edge);
+    }
+    fclose($file);
 }
 
-function load_arr($filename) {
-    $fileContents = file_get_contents($filename);
-    $decoded = json_decode($fileContents, true);
-    return $decoded;
+function get_token($path) {
+    $token_file = fopen("access_token", "r");
+    $access_token = fread($token_file, filesize("access_token"));
+    return $access_token;
 }
 
 $vk = new VKApiClient();
-$token_file = fopen("access_token", "r");
-$access_token = fread($token_file, filesize("access_token"));
+$access_token = get_token('access_token');
 
 $collected_groups = array();
 $collected_edges_arr = array();
+save_arr_csv('data/data.csv', $collected_edges_arr, 'w');
+$counter = 0;
 
 
 do {
@@ -33,27 +40,44 @@ do {
             "fields" => array("members_count")
         ));
     foreach ($groups_response as $item) {
-        if (!array_key_exists("deactivated", $item) and $item["type"] == "group" and $item["members_count"] > 100) {
-            if (!array_key_exists($item["id"], $collected_groups)) {
-                usleep(333333);
-                $collected_groups[$item["id"]] = 1;
-                $users_response = $vk->groups()->getMembers($access_token, array(
-                    "group_id" => $item["id"]
-                ));
-                $users = $users_response["items"];
-                foreach($users as $user) {
-                    array_push($collected_edges_arr, array($item["id"], $user));
+        if (!array_key_exists($item["id"], $collected_groups) and !array_key_exists("deactivated", $item) and $item["type"] == "group" and $item["members_count"] > 100) {
+            $collected_groups[$item["id"]] = 1;
+            $counter += 1;
+            $offset = 0;
+            do {
+                try {
+                    usleep(333333);
+                    $users_response = $vk->groups()->getMembers($access_token, array(
+                        "group_id" => $item["id"],
+                        "offset" => $offset
+                    ));
+                    $users = $users_response["items"];
+                    foreach ($users as $user) {
+                        array_push($collected_edges_arr, array($item["id"], $user));
+                    }
+                    $offset += 1000;
                 }
+                catch (VKApiAccessGroupException $e) {
+                    echo "private group exception\n";
+                    break;
+                }
+                catch (VKApiServerException $e1) {
+                    echo "server exception\n";
+                    sleep(60);
+                }
+                catch (VKClientException $e2) {
+                    echo "client exception\n";
+                    sleep(60);
+                }
+            } while (count($users) == 1000);
+            if ($counter % 100 == 0) {
+                save_arr_csv("data/data.csv", $collected_edges_arr, 'a');
+                $collected_edges_arr = array();
             }
         }
     }
 } while (count($collected_groups) < 30000 or count($collected_edges_arr) < 20000000);
 
 
-$file = fopen('data.csv', 'w');
-fputcsv($file, array('group', 'user', 'target'));
-foreach($collected_edges_arr as $edge) {
-    fputcsv($file, $edge);
-}
-fclose($file);
+save_arr_csv("data/data.csv", $collected_edges_arr, 'a');
 ?>
